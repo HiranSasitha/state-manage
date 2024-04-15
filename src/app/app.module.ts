b07ba5b276f0ad5e731006982f44d7bf026a68dc
HiranSasitha <hiransasitha123@gamil.com>
2024-04-16 00:18:51 +0530

    completed new task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NewTaskComponent
 
   ],
   imports: [
